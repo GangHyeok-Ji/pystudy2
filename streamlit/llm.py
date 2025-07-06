5659e745df4218e930562b25f64a669691f44354
KangHyoek <ghjee82@gmail.com>
2025-07-06 21:04:21 +0900

    4-3 마무리

--- streamlit/llm.py
+++ streamlit/llm.py
@@ -1,25 +1,32 @@
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import RetrievalQA
-from langchain import hub
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 load_dotenv()
 
-def get_ai_msg(user_message):
-
+def get_retriever():
     embedding = OpenAIEmbeddings(model='text-embedding-3-large')
     index_name='tax-markdown-index'
     database = PineconeVectorStore(index_name=index_name, embedding=embedding)        
-
-    llm = ChatOpenAI(model='gpt-4o')
-    prompt = hub.pull("rlm/rag-prompt")
     retriever = database.as_retriever(search_kwargs={'k': 4})
-    
-    qa_chain = RetrievalQA.from_chain_type(        llm,         retriever=database.as_retriever(),        chain_type_kwargs={"prompt": prompt}    )
+
+    return retriever
+
+def get_llm(model='gpt-4o'):
+    llm = ChatOpenAI(model=model)
+
+    return llm
+
+def get_dictionary_chain():
+    llm = get_llm()
     dictionary = ["사람을 나타내는 표현 -> 거주자"]
 
     prompt = ChatPromptTemplate.from_template(f"""
@@ -32,6 +39,84 @@ def get_ai_msg(user_message):
     """)
 
     dictionary_chain = prompt | llm | StrOutputParser()
-    tax_chain = {"query": dictionary_chain} | qa_chain
-    ai_message = tax_chain.invoke({"question": user_message})
-    return ai_message["result"]
+    
+    return dictionary_chain
+
+def get_rag_chain():
+    llm = get_llm()
+    retriever = get_retriever()
+    
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, "
+        "just reformulate it if needed and otherwise return it as is."
+    )
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    contextualize_q_llm = llm.with_config(tags=["contextualize_q_llm"])
+    history_aware_retriever = create_history_aware_retriever(
+        contextualize_q_llm, retriever, contextualize_q_prompt
+    )
+
+    system_prompt = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        "{context}"
+    )
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+ 
+
+    store = {}
+
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = ChatMessageHistory()
+        return store[session_id]
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    ).pick('answer')
+
+    return conversational_rag_chain
+
+def get_ai_response(user_message):
+
+    dictionary_chain = get_dictionary_chain()
+    rag_chain = get_rag_chain()
+
+    tax_chain = {"input": dictionary_chain} | rag_chain
+    ai_response = tax_chain.stream(
+        {
+            "question": user_message
+        },
+        config={
+            "configurable": {"session_id" : "abc123"}
+        },
+    )
+    return ai_response
